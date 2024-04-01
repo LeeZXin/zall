d5aae02a15d7362bf5e68d406b006c9c97a9c488
lizexin <546973743@qq.com>
2024-04-01 18:57:51 +0800

    feat: some

--- cmd/app.go
+++ cmd/app.go
@@ -17,6 +17,7 @@ var (
 		FileServer,
 		Probe,
 		DeployServer,
+		PromAgent,
 	}
 )
 
---
+++ cmd/promagent.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/LeeZXin/zall/promagent/agent"
+	"github.com/LeeZXin/zsf/zsf"
+	"github.com/urfave/cli/v2"
+)
+
+var PromAgent = &cli.Command{
+	Name:        "actionAgent",
+	Usage:       "This command starts prom agent",
+	Description: "promAgent provides etcd discovery for prometheus",
+	Action:      runPromAgent,
+}
+
+func runPromAgent(*cli.Context) error {
+	agent.StartAgent()
+	zsf.Run()
+	return nil
+}
